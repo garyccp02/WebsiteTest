b26e955ed4b666059a62792e9469510e701885a8
hell0venus <48729877+garyccp02@users.noreply.github.com>
2021-12-13 14:02:08 +0800

    add page mini-car

--- js/r.js
+++
@@ -1,7 +0,0 @@
-function myFunction1() {
-    document.getElementById("fakeBCSMemberImg1").style.display = "block";
-}
-
-function myFunction2() {
-    document.getElementById("fakeBCSMemberImg2").style.display = "block";
-}
